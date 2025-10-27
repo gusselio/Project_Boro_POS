a44465f00cef1df38b71de07a8d6cfd9f73ebbab
Gustavo Leal <guselio98@gmail.com>
2025-10-26 18:23:38 -0600

    Ficha tecnica del proyecto

---
+++ main.py
@@ -0,0 +1,7 @@
+# =========================================
+# Universidad Tecmilenio
+# Proyecto: Sistema de Inventario - Café BORO
+# Versión: Avance lineal (sin clases)
+# Nombre: Gustavo Salvador Leal Dominguez
+# Fecha: 26 de Octubre de 2025
+# =========================================
