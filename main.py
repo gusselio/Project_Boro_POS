03a107b762ea162c58e432c1251d351991547f5c
Gustavo Leal <guselio98@gmail.com>
2025-10-26 18:26:42 -0600

    Creacion del diccionario para inventario

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 # Nombre: Gustavo Salvador Leal Dominguez
 # Fecha: 26 de Octubre de 2025
 # =========================================
+
+inventario = {
+    "Cafe": 10,     #Kilos
+    "Leche": 5000,  #Mililitros
+    "Azucar": 2000, #Gramos
+    "Vasos": 100,   #Unidades
+    "Tapas": 100    #Unidades
+}
